c0f0a832f25f66dceadc2f66859ff5b955fa4e56
David Betteridge <DavidaBetteridge@gmail.com>
2017-09-24 21:23:01 +0100

    POP and PUSH commands

--- z80vm.Tests/StackTests.cs
+++ z80vm.Tests/StackTests.cs
@@ -30,14 +30,64 @@ namespace z80vm.Tests
             var machine = new Machine();
 
             var currentValueOfSP = machine.Registers.Read(Reg16.SP);
-            machine.Registers.Set(register, 0xABCD);  
+            machine.Registers.Set(register, 0xABCD);
             machine.PUSH(register);
 
-            Assert.Equal(machine.Memory.Read(currentValueOfSP), 0xAB);
-            Assert.Equal(machine.Memory.Read(--currentValueOfSP), 0xCD);
+            currentValueOfSP = (ushort)(currentValueOfSP - 1);
+            Assert.Equal(0xAB, machine.Memory.Read(currentValueOfSP));
+
+            currentValueOfSP = (ushort)(currentValueOfSP - 1);
+            Assert.Equal(0xCD, machine.Memory.Read(currentValueOfSP));
         }
 
+        [Fact]
+        public void PoppingAValueFromToTheStackShouldIncreaseTheSPRegisterByTwo()
+        {
+            var machine = new Machine();
+            machine.PUSH(Reg16.AF);
 
+            var currentValueOfSP = machine.Registers.Read(Reg16.SP);
+            machine.POP(Reg16.AF);
 
+            Assert.Equal(currentValueOfSP + 2, machine.Registers.Read(Reg16.SP));
+        }
+
+        [Theory]
+        [InlineData(Reg16.AF)]
+        [InlineData(Reg16.BC)]
+        [InlineData(Reg16.DE)]
+        [InlineData(Reg16.HL)]
+        [InlineData(Reg16.IX)]
+        [InlineData(Reg16.IY)]
+        public void PoppingAValueFromTheStackCopiesTheMemoryLocationPointedToBySPToTheRegister(Reg16 register)
+        {
+            //The Z80 is little endian,  so the lowest byte is stored in the lowest address
+            var machine = new Machine();
+            machine.Registers.Set(Reg16.SP, 0x1000);
+            machine.Memory.Set(0x1000 + 1, 0xAA);  //High order byte
+            machine.Memory.Set(0x1000, 0xBB);  //Low order byte
+
+            machine.POP(register);
+
+            Assert.Equal(0xAABB, machine.Registers.Read(register));
+        }
+
+        [Theory]
+        [InlineData(Reg16.AF)]
+        [InlineData(Reg16.BC)]
+        [InlineData(Reg16.DE)]
+        [InlineData(Reg16.HL)]
+        [InlineData(Reg16.IX)]
+        [InlineData(Reg16.IY)]
+        public void ShouldBeAbleToPOPaPUSHedValue(Reg16 register)
+        {
+            var machine = new Machine();
+            machine.Registers.Set(register, 0xAAAA);
+            machine.PUSH(register);
+            machine.Registers.Set(register, 0xBBBB);
+            machine.POP(register);
+
+            Assert.Equal(0xAAAA, machine.Registers.Read(register));
+        }
     }
 }
--- z80vm/Machine.cs
+++ z80vm/Machine.cs
@@ -15,21 +15,46 @@ namespace z80vm
             this.Flags = new Flags(this.Registers);
         }
 
+        /// <summary>
+        /// SP is decreased by two and the value of reg16 is copied to the memory location pointed by the new value of SP. 
+        /// It does not affect the flags.
+        /// </summary>
+        /// <param name="register"></param>
         public void PUSH(Reg16 register)
         {
-            //The Z80 is little endian,  so the lowest byte is stored in the lowest address
             var value = this.Registers.Read(register);
             var (highOrderByte, lowOrderByte) = value.Split();
 
-            var sp = this.Registers.Read(Reg16.SP);
-            this.Memory.Set(sp, highOrderByte);
-            this.Memory.Set((ushort)(sp - 1), lowOrderByte);
-            this.Registers.Set(Reg16.SP, (ushort)(sp - 2));
+            var stackPointer = this.Registers.Read(Reg16.SP);
+            stackPointer = (ushort)(stackPointer - 2);
+            this.Registers.Set(Reg16.SP, stackPointer);
+
+            //The Z80 is little endian,  so the lowest byte is stored in the lowest address
+            this.Memory.Set((ushort)(stackPointer + 1), highOrderByte);
+            this.Memory.Set((ushort)(stackPointer), lowOrderByte);
         }
 
         public void LD(Reg8 register, byte value)
         {
             this.Registers.Set(register, value);
+        }
+
+        /// <summary>
+        /// The value of the word found at the memory location pointed by SP is copied into reg16, then SP is increased by 2. 
+        /// No flags are affected (except for the case of popping into AF).
+        /// </summary>
+        /// <param name="register"></param>
+        public void POP(Reg16 register)
+        {
+            var stackPointer = this.Registers.Read(Reg16.SP);
+
+            var highOrderByte = this.Memory.Read((ushort)(stackPointer + 1));
+            var lowOrderByte = this.Memory.Read((ushort)(stackPointer));
+            var fullByte = (ushort)((ushort)(highOrderByte << 8) | lowOrderByte);
+
+            this.Registers.Set(Reg16.SP, (ushort)(stackPointer + 2));
+            this.Registers.Set(register, fullByte);
+
         }
     }
 }
