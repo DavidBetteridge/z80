33f8a783ff242997d6e225a010334ac23e113cf0
David Betteridge <DavidaBetteridge@gmail.com>
2017-09-24 18:00:35 +0100

    Refactored flags code

--- z80vm/Extensions.cs
+++ z80vm/Extensions.cs
@@ -1,4 +1,6 @@
-﻿namespace z80vm
+﻿using System;
+
+namespace z80vm
 {
     static class Extensions
     {
@@ -13,6 +15,42 @@
             var lowOrderByte = (byte)(value & 0x00FF);
 
             return (highOrderByte, lowOrderByte);
+        }
+
+        /// <summary>
+        /// Reads a single bit from the byte
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="bitNumber">7 6 5 4 3 2 1 0</param>
+        /// <returns></returns>
+        public static bool ReadBit(this byte value, int bitNumber)
+        {
+            if (bitNumber < 0 || bitNumber > 7) throw new ArgumentOutOfRangeException(nameof(bitNumber), "Must be between 0 and 7");
+            return (value & (1 << bitNumber)) != 0;
+        }
+
+        /// <summary>
+        /// Returns a new byte based on the supplied value but with the specified bit reset to 0
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="bitNumber">7 6 5 4 3 2 1 0</param>
+        /// <returns></returns>
+        public static byte ClearBit(this byte value, int bitNumber)
+        {
+            if (bitNumber < 0 || bitNumber > 7) throw new ArgumentOutOfRangeException(nameof(bitNumber), "Must be between 0 and 7");
+            return (byte)(value & ~(1 << bitNumber));
+        }
+
+        /// <summary>
+        /// Returns a new byte based on the supplied value but with the specified bit set to 1
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="bitNumber">7 6 5 4 3 2 1 0</param>
+        /// <returns></returns>
+        public static byte SetBit(this byte value, int bitNumber)
+        {
+            if (bitNumber < 0 || bitNumber > 7) throw new ArgumentOutOfRangeException(nameof(bitNumber), "Must be between 0 and 7");
+            return (byte)(value | (1 << bitNumber));
         }
     }
 }
--- z80vm/Flags.cs
+++ z80vm/Flags.cs
@@ -1,21 +1,61 @@
-﻿using System;
-
-namespace z80vm
+﻿namespace z80vm
 {
+    /// <summary>
+    /// Specifies which flag (bit in the 'A' register) we are dealing with
+    /// </summary>
     public enum Flag
     {
+        /// <summary>
+        /// the Carry flag. It is set when the last operation caused a register to step over zero in either direction.
+        /// </summary>
         C = 0,
+
+        /// <summary>
+        /// this bit indicates subtraction.
+        /// </summary>
         N = 1,
+
+        /// <summary>
+        /// The Parity/Overflow flag, which has different meanings depending on which instruction alters it. When it is Parity, it tells you whether the number of set (1) bits in the register is odd or even. When it means Overflow, it indicates overflowing (leaving the 0...127 or -128...-1 intervals) by being set. Additional, in the case of certain operations the contents of the flip-flop controlling the interrupt also appear here.
+        /// </summary>
         PV = 2,
+
+        /// <summary>
+        /// undefined
+        /// </summary>
         Undefined3 = 3,
+
+        /// <summary>
+        /// the BCD Half Carry flag. It is set when there is a carry transfer from bit 3 to bit 4, and it is used for packed BCD correction
+        /// </summary>
         H = 4,
+
+        /// <summary>
+        /// undefined
+        /// </summary>
         Undefined5 = 5,
+
+        /// <summary>
+        /// the Zero flag, which is set if one of the registers was set to zero during the preceding operation. Note that not all operations resulting in zero alter this flag!
+        /// </summary>
         Z = 6,
+
+        /// <summary>
+        /// the Sign flag, which is set if one of the registers changed its value from non-negative to negative. It is zero in all the other cases.
+        /// </summary>
         S = 7
     }
 
+    /// <summary>
+    /// Flags are bits in the 'A' register.  Each bit has a different meaning
+    /// and is set/used by various opcodes
+    /// </summary>
     public class Flags
     {
+        /// <summary>
+        /// We need a reference back to the registers so that we can
+        /// read/set the 'A' register
+        /// </summary>
         private readonly Registers registers;
 
         public Flags(Registers registers)
@@ -23,87 +63,37 @@ namespace z80vm
             this.registers = registers;
         }
 
+        /// <summary>
+        /// Sets the corresponding bit in the 'A' register to 1
+        /// </summary>
+        /// <param name="flag"></param>
         public void Set(Flag flag)
         {
-            var a = this.registers.Read(Reg8.A);
-
-            switch (flag)
-            {
-                case Flag.C:
-                    a |= 0b0000_0001;
-                    break;
-                case Flag.N:
-                    a |= 0b0000_0010;
-                    break;
-                case Flag.PV:
-                    a |= 0b0000_0100;
-                    break;
-                case Flag.Undefined3:
-                    a |= 0b0000_1000;
-                    break;
-                case Flag.H:
-                    a |= 0b0001_0000;
-                    break;
-                case Flag.Undefined5:
-                    a |= 0b0010_0000;
-                    break;
-                case Flag.Z:
-                    a |= 0b0100_0000;
-                    break;
-                case Flag.S:
-                    a |= 0b1000_0000;
-                    break;
-                default:
-                    break;
-            }
-
-            this.registers.Set(Reg8.A, a);
-
+            var valueOfRegisterA = this.registers.Read(Reg8.A);
+            valueOfRegisterA = valueOfRegisterA.SetBit((int)flag);
+            this.registers.Set(Reg8.A, valueOfRegisterA);
         }
 
+        /// <summary>
+        /// Sets the corresponding bit in the 'A' register to 0
+        /// </summary>
+        /// <param name="flag"></param>
         public void Clear(Flag flag)
         {
-            var a = this.registers.Read(Reg8.A);
-
-            switch (flag)
-            {
-                case Flag.C:
-                    a &= 0b1111_1110;
-                    break;
-                case Flag.N:
-                    a &= 0b1111_1101;
-                    break;
-                case Flag.PV:
-                    a &= 0b1111_1011;
-                    break;
-                case Flag.Undefined3:
-                    a &= 0b1111_0111;
-                    break;
-                case Flag.H:
-                    a &= 0b1110_1111;
-                    break;
-                case Flag.Undefined5:
-                    a &= 0b1101_1111;
-                    break;
-                case Flag.Z:
-                    a &= 0b1011_1111;
-                    break;
-                case Flag.S:
-                    a &= 0b0111_1111;
-                    break;
-                default:
-                    break;
-            }
-
-            this.registers.Set(Reg8.A, a);
-
+            var valueOfRegisterA = this.registers.Read(Reg8.A);
+            valueOfRegisterA = valueOfRegisterA.ClearBit((int)flag);
+            this.registers.Set(Reg8.A, valueOfRegisterA);
         }
 
+        /// <summary>
+        /// Reads the value of the corresponding bit in the 'A' register
+        /// </summary>
+        /// <param name="flag"></param>
+        /// <returns></returns>
         public bool Read(Flag flag)
         {
             var a = this.registers.Read(Reg8.A);
-
-            return (a & (1 << (int)flag)) != 0;
+            return a.ReadBit((int)flag);
         }
     }
 }
