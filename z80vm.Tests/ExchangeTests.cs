f6d5458f1175b83aed6e1072817a4aa10433a203
DavidBetteridge <davidabetteridge@gmail.com>
2017-09-25 08:39:16 +0100

    Added EXX (exchange) command

---
+++ z80vm.Tests/ExchangeTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace z80vm.Tests
+{
+    public class ExchangeTests
+    {
+        [Theory]
+        [InlineData(Reg16.BC, Reg16Shadow.BC)]
+        [InlineData(Reg16.DE, Reg16Shadow.DE)]
+        [InlineData(Reg16.HL, Reg16Shadow.HL)]
+        public void EXXShouldSwapARegisterWithItsShadow(Reg16 register, Reg16Shadow shadow)
+        {
+            var machine = new Machine();
+
+            machine.Registers.Set(register, 0xAAAA);
+            machine.Registers.Set(shadow, 0xBBBB);
+            machine.EXX();
+
+            Assert.Equal(0xAAAA, machine.Registers.Read(shadow));
+            Assert.Equal(0xBBBB, machine.Registers.Read(register));
+        }
+    }
+}
--- z80vm/Machine.cs
+++ z80vm/Machine.cs
@@ -16,6 +16,23 @@ namespace z80vm
         }
 
         /// <summary>
+        /// There are no operands. This instruction exchanges BC with BC’, DE with DE’ and HL with HL’ at the same time. 
+        /// </summary>
+        public void EXX()
+        {
+            void swap(Reg16 register, Reg16Shadow shadow)
+            {
+                var temp = this.Registers.Read(register);
+                this.Registers.Set(register, this.Registers.Read(shadow));
+                this.Registers.Set(shadow, temp);
+            }
+
+            swap(Reg16.BC, Reg16Shadow.BC);
+            swap(Reg16.DE, Reg16Shadow.DE);
+            swap(Reg16.HL, Reg16Shadow.HL);
+        }
+
+        /// <summary>
         /// Usage: Adds 2 numbers together and stores the result in the first operand
         /// Flags: Preserves the S, Z and P/V flags, and H is undefined. Rest of flags modified by definition.
         /// </summary>
