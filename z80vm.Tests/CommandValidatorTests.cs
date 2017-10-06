ab462612d4e416f8424afca4b0900a8e95018b0b
David Betteridge <DavidaBetteridge@gmail.com>
2017-10-06 23:24:07 +0100

    More command validator improvements

---
+++ z80vm.Tests/CommandValidatorTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace z80vm.Tests
+{
+    public class CommandValidatorTests
+    {
+        [Fact]
+        public void TheCommand_Add_a_a_Is_Valid()
+        {
+            var cv = new CommandValidator();
+            cv.EnsureCommandIsValid(Reg8.A, Reg8.A, "ADD");
+
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void TheCommand_Add_b_a_Is_NotValid()
+        {
+            var cv = new CommandValidator();
+            var exception = Record.Exception(() => cv.EnsureCommandIsValid(Reg8.B, Reg8.A, "ADD"));
+            Assert.IsType(typeof(System.InvalidOperationException), exception);
+        }
+    }
+}
--- z80vm/CommandValidator.cs
+++ z80vm/CommandValidator.cs
@@ -1,24 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 
 namespace z80vm
 {
+    /// <summary>
+    /// A CommandValidator checks that the operands provided for a command are valid for that instruction.
+    /// For example,  when adding an 8bit number,  the only valid value for the first operand is Reg8.A
+    /// Instead of filling the source code with a lot of IF/SWITCH statements we stored the list of valid
+    /// commands in an embedded text file called AllowedInstructions.txt
+    /// </summary>
     public class CommandValidator : ICommandValidator
     {
+        private HashSet<string> lines;
+
+        /// <summary>
+        /// Checks that the command is valid with these two operands.  Throws an exception if not
+        /// </summary>
+        /// <param name="operand1">The value of the first parameter</param>
+        /// <param name="operand2">The value of the second parameter</param>
+        /// <param name="command">The name of the command,  or the calling method is not supplied.  For example SUB</param>
         public void EnsureCommandIsValid(object operand1, object operand2, [System.Runtime.CompilerServices.CallerMemberName] string command = "")
         {
-            var assembly = typeof(Machine).GetTypeInfo().Assembly;
-            var resourceName = "z80vm.AllowedInstructions.txt";
-            using (var stream = assembly.GetManifestResourceStream(resourceName))
-            using (var reader = new StreamReader(stream))
+            if (lines == null)
             {
-                var result = reader.ReadToEnd();
-                var lines = result.Split(new[] { System.Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
-                command = command.ToLower() + " " + $"{operand1.ToString()},{operand2.ToString()}".ToLower();
-                if (!lines.Contains(command)) throw new InvalidOperationException("Invalid operand combination - " + command);
+                var assembly = typeof(Machine).GetTypeInfo().Assembly;
+                var resourceName = "z80vm.AllowedInstructions.txt";
+                using (var stream = assembly.GetManifestResourceStream(resourceName))
+                using (var reader = new StreamReader(stream))
+                {
+                    var result = reader.ReadToEnd();
+                    var linesInFile = result
+                                        .Split(new[] { System.Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
+                                        .Select(l => l.Trim())
+                                        .Where(l => !string.IsNullOrWhiteSpace(l));
+                    this.lines = new HashSet<string>(linesInFile);
+                }
             }
+
+            command = command.ToLower() + " " + $"{operand1.ToString()},{operand2.ToString()}".ToLower();
+            if (!lines.Contains(command)) throw new InvalidOperationException("Invalid operand combination - " + command);
         }
     }
 }
